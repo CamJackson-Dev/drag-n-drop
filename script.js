78251f76db201b9283e4b8691c50ab1a6542cec4
Cam Jackson <cameroncj86@gmail.com>
2020-10-26 13:00:04 +1000

    Add Video BG

--- script.js
+++ script.js
@@ -152,16 +152,20 @@ function hideInputBox(column) {
 
 // Allows arrays to reflect Drag and Drop items
 function rebuildArrays() {
+  // console.log(backlogListEl.children);
+  // Same functionality as below using the map method
   backlogListArray = Array.from(backlogListEl.children).map(
     (i) => i.textContent
   );
   progressListArray = Array.from(progressListEl.children).map(
     (i) => i.textContent
   );
-  completedListArray = Array.from(completedListEl.children).map(
+  completedListArray = Array.from(completeListEl.children).map(
     (i) => i.textContent
   );
   onHoldListArray = Array.from(onHoldListEl.children).map((i) => i.textContent);
+
+  // Using For Loop
 
   // backlogListArray = [];
   // for (let i = 0; i < backlogListEl.children.length; i++) {
